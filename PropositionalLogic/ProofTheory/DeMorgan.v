1b9f05b05c7570a0ee58b6f7eb9e22082ba5c88f
Xiangtao Zeng <3170345463@qq.com>
2020-01-13 19:10:48 +0800

    finish DeMorgan

--- PropositionalLogic/ProofTheory/DeMorgan.v
+++ PropositionalLogic/ProofTheory/DeMorgan.v
@@ -11,7 +11,7 @@ Local Open Scope logic_base.
 Local Open Scope syntax.
 Import PropositionalLanguageNotation.
 
-Class DeMorganPropositionalLogic (L: Language) {minL: MinimumLanguage L} {pL: PropositionalLanguage L} (Gamma: Provable L) {minAX: MinimumAxiomatization L Gamma} {ipAX: IntuitionisticPropositionalLogic L Gamma} := {
+Class DeMorganPropositionalAxiomatization (L: Language) {minL: MinimumLanguage L} {orpL: OrpLanguage L} {falsepL: FalsepLanguage L} {negpL: NegpLanguage L} (Gamma: Provable L) {minAX: MinimumAxiomatization L Gamma} {orpGamma: OrpAxiomatization L Gamma} {falsepGamma: FalsepAxiomatization L Gamma} {inegpGamma: IntuitionisticNegpAxiomatization L Gamma} := {
   weak_excluded_middle: forall x, |-- ~~ x || ~~ ~~ x
 }.
 
@@ -19,11 +19,21 @@ Section DeMorgan.
 
 Context {L: Language}
         {minL: MinimumLanguage L}
-        {pL: PropositionalLanguage L}
+        {andpL: AndpLanguage L}
+        {orpL: OrpLanguage L}
+        {falsepL: FalsepLanguage L}
+        {negpL: NegpLanguage L}
+        {iffpL: IffpLanguage L}
+        {truepL: TruepLanguage L}
         {Gamma: Provable L}
         {minAX: MinimumAxiomatization L Gamma}
-        {ipAX: IntuitionisticPropositionalLogic L Gamma}
-        {dmpAX: DeMorganPropositionalLogic L Gamma}.
+        {andpGamma: AndpAxiomatization L Gamma}
+        {orpGamma: OrpAxiomatization L Gamma}
+        {falsepGamma: FalsepAxiomatization L Gamma}
+        {inegpGamma: IntuitionisticNegpAxiomatization L Gamma}
+        {iffpGamma: IffpAxiomatization L Gamma}
+        {truepGamma: TruepAxiomatization L Gamma}
+        {dmpAX: DeMorganPropositionalAxiomatization L Gamma}.
 
 Lemma demorgan_negp_andp: forall (x y: expr),
   |-- ~~ (x && y) <--> (~~ x || ~~ y).
@@ -31,22 +41,26 @@ Proof.
   AddSequentCalculus.
   intros.
   rewrite provable_derivable.
-  apply deduction_andp_intros; [| rewrite <- provable_derivable; apply demorgan_orp_negp].
-  rewrite <- deduction_theorem.
-  apply (deduction_modus_ponens _ (~~ x || ~~ ~~ x)); [apply deduction_weaken0, weak_excluded_middle |].
-  apply deduction_orp_elim'.
-  + apply deduction_weaken0.
-    apply orp_intros1.
-  + rewrite <- deduction_theorem.
-    apply deduction_orp_intros2.
-    unfold negp at 4.
-    rewrite <- deduction_theorem.
-    apply (deduction_modus_ponens _ (x --> FF)).
+  apply deduction_iffp_intros.
+  + apply (deduction_modus_ponens _ (~~ x || ~~ ~~ x)); [apply deduction_weaken0, weak_excluded_middle |].
+    apply deduction_orp_elim'.
+    - apply deduction_weaken0.
+      apply orp_intros1.
     - rewrite <- deduction_theorem.
-      apply (deduction_modus_ponens _ (x && y)).
-      * apply deduction_andp_intros; [| apply deduction_weaken1]; apply derivable_assum1.
-      * do 3 apply deduction_weaken1; apply derivable_assum1.
-    - apply deduction_weaken1; apply derivable_assum1.
+      apply deduction_orp_intros2.
+      pose proof negp_fold y. rewrite <- H.
+      rewrite <- deduction_theorem.
+      apply  (deduction_modus_ponens _ (x --> FF)).
+      * rewrite <- deduction_theorem.
+        apply (deduction_modus_ponens _ (x && y)).
+        { apply deduction_andp_intros; [| apply deduction_weaken1]; apply derivable_assum1. }
+        { pose proof negp_unfold (x && y). rewrite <- H0. solve_assum. }
+      * pose proof negp_fold x. rewrite H0.
+        pose proof negp_unfold (~~x). rewrite <- H1.
+        solve_assum.
+  + rewrite deduction_theorem.
+    rewrite <- provable_derivable.
+    apply demorgan_orp_negp.
 Qed.
 
 End DeMorgan.
