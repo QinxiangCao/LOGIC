769f2398300c337179c6d4497593f7925933ef56
Xiangtao Zeng <3170345463@qq.com>
2020-01-16 17:43:46 +0800

    add new file about Propositional Connectives Interconversion

---
+++ PropositionalLogic/ProofTheory/TheoryOfPropositionalConnectives.v
@@ -0,0 +1,11 @@
+(*TODO: there should be
+  and   + neg  = or
+  or    + neg  = and
+  neg   + or   = imp
+  and   + imp  = iff
+  false + imp  = true
+  true  + imp  = false
+  neg   + true = false
+  neg   + true = false
+  neg   + imp  = or
+if possible, write about from Coq prop to true and false*)
