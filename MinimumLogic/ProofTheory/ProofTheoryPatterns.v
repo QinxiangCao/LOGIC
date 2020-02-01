6ed27287a63bb76e55516e3986e7341921f96d5e
Qinxiang <caoqinxiang@gmail.com>
2020-02-02 04:20:36 +0800

    Missing one file in a couple of commits before.

---
+++ MinimumLogic/ProofTheory/ProofTheoryPatterns.v
@@ -0,0 +1,5 @@
+Require Logic.GeneralLogic.ProofTheory.ProofTheoryPatternsD1.
+Require Logic.MinimumLogic.ProofTheory.ProofTheoryPatternsP.
+
+Module D1 := ProofTheoryPatternsD1.
+Module P := ProofTheoryPatternsP.
