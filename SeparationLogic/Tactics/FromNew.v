56617e7285102c37a0d6c7fb8042a574867c5a30
Xiangtao Zeng <3170345463@qq.com>
2020-09-27 18:47:43 +0800

    finish proofs

--- SeparationLogic/Tactics/FromNew.v
+++ SeparationLogic/Tactics/FromNew.v
@@ -35,17 +35,6 @@ Ltac pred n :=
   | S ?m => m
   end.
 
-Ltac reverse_cont l k :=
-  match l with
-  | @nil ?T => k (@nil T)
-  | @cons _ ?h ?t =>
-    let k' l :=
-        let t' := k l in
-        constr:(cons h t')
-    in reverse_cont t k'
-  end.
-Ltac reverse l := reverse_cont l ltac:(fun l => l).
-
 Ltac search_expr' n i l l0 :=
   match l with
   | nil => let len := length l0 in constr:((S len, n :: l0))
@@ -53,14 +42,6 @@ Ltac search_expr' n i l l0 :=
   | _ :: ?t => let pi := pred i in search_expr' n pi t l0
   end.
 Ltac search_expr n l := let len := length l in search_expr' n len l l.
-
-Fixpoint reflect (de : expr_deep) (tbl : list expr) (default : expr) : expr :=
-    match de with
-    | varp_deep n => nth (pred n) tbl default
-    | sepcon_deep e1 e2 => (reflect e1 tbl default) * (reflect e2 tbl default )
-    | impp_deep e1 e2 => (reflect e1 tbl default) --> (reflect e2 tbl default)
-    | emp_deep => emp
-    end.
 
 Ltac shallowTodeep' se l0 :=
   match se with
@@ -228,17 +209,6 @@ Module PTree.
 
   Definition empty {A : Type} := (Leaf : tree A).
 
-  Fixpoint get (A : Type) (i : positive) (m : tree A) {struct i} : option A :=
-    match m with
-    | Leaf => None
-    | Node l o r =>
-        match i with
-        | xH => o
-        | xO ii => get A ii l
-        | xI ii => get A ii r
-        end
-    end.
-
   Fixpoint get_rec' (A : Type) (i : positive) (m : tree A) (f : tree A -> tree A) : tree A :=
     match i with
     | xH => f m
@@ -255,22 +225,6 @@ Module PTree.
     match get_rec' A i m (fun m0 : tree A => m0) with
     | Leaf => None
     | Node l o r => o
-    end.
-
-  Fixpoint set (A : Type) (i : positive) (v : A) (m : tree A) {struct i} : tree A :=
-    match m with
-    | Leaf =>
-        match i with
-        | xH => Node Leaf (Some v) Leaf
-        | xO ii => Node (set A ii v Leaf) None Leaf
-        | xI ii => Node Leaf None (set A ii v Leaf)
-        end
-    | Node l o r =>
-        match i with
-        | xH => Node l (Some v) r
-        | xO ii => Node (set A ii v l) o r
-        | xI ii => Node l o (set A ii v r)
-        end
     end.
 
   Fixpoint set_rec' (A : Type) (i : positive) (v : A) (m : tree A) (f : tree A -> tree A) : tree A :=
@@ -302,20 +256,6 @@ Module PTree.
   Definition elements (A : Type) (m : tree A) :=
   xelements A m nil.
 
-  Goal forall A p x,
-  elements A (set A p x empty) = x :: nil.
-  intros.
-  induction p.
-  - compute.
-    fold set. fold xelements.
-    auto.
-  - compute.
-    fold set. fold xelements.
-    auto.
-  - compute.
-    auto.
-  Qed.
-
   Lemma get_empty : forall A p,
   get_rec A p empty = None.
 Proof.
@@ -327,33 +267,11 @@ Proof.
   clearbody f.
   revert f H.
   induction p; intros.
-  - unfold get_rec'; fold get_rec'.
-    set (f' := (fun m0 : tree A => match f m0 with
-                                        | Leaf => Leaf
-                                        | Node _ _ r => r
-                                        end)).
-    assert (f' Leaf = Leaf).
-    unfold f'. rewrite H. auto.
-    apply IHp, H0.
-  - unfold get_rec'; fold get_rec'.
-    set (f' := (fun m0 : tree A => match f m0 with
-                                        | Leaf => Leaf
-                                        | Node l _ _ => l
-                                        end)).
-    assert (f' Leaf = Leaf).
-    unfold f'. rewrite H. auto.
-    apply IHp, H0.
-  - compute.
-    rewrite H.
-    auto.
+  - apply IHp. rewrite H. auto.
+  - apply IHp. rewrite H. auto.
+  - compute. rewrite H. auto.
 Qed.
-(*
-  Lemma get_elements_general : forall A p m x,
-  get_rec A p m = Some x ->
-  In x (elements A m).
-Proof.
-Admitted.
-*)
+
   Lemma elements_set_empty : forall A p x,
   elements A (set_rec A p x empty) = x :: nil.
 Proof.
@@ -365,33 +283,226 @@ Proof.
   clearbody f.
   revert f H.
   induction p; intros.
-  - unfold set_rec', empty. fold set_rec'.
-    apply IHp.
-    intros t'; rewrite <- H.
+  - apply IHp.
+    intros; rewrite <- H.
     subst; auto.
-  - unfold set_rec', empty. fold set_rec'.
-    apply IHp.
-    intros t'; rewrite <- H.
+  - apply IHp.
+    intros; rewrite <- H.
     subst; auto.
   - unfold set_rec', empty.
     rewrite <- H.
     subst; auto.
 Qed.
 
-  Lemma elements_set_none : forall A p x m, exists l1 l2,
+  Lemma get_node0 : forall A p m1 o m2,
+  (get_rec A p~0 (Node m1 o m2)) = (get_rec A p m1).
+Proof.
+  intros.
+  unfold get_rec, get_rec'; fold get_rec'.
+  set (f1 := fun m0 : tree A => match m0 with
+                        | Leaf => Leaf
+                        | Node l _ _ => l
+                        end).
+  set (f2 := fun m0 : tree A => m0).
+  assert (forall m1 o m2, f1 (Node m1 o m2) = f2 m1).
+  subst; auto.
+  clearbody f1 f2.
+  revert f1 f2 H.
+  induction p; intros.
+  - unfold get_rec'; fold get_rec'.
+    apply IHp.
+    intros; rewrite H. auto.
+  - unfold get_rec'; fold get_rec'.
+    apply IHp.
+    intros; rewrite H. auto.
+  - compute. rewrite H. auto.
+Qed.
+
+  Lemma get_node1 : forall A p m1 o m2,
+  (get_rec A p~1 (Node m1 o m2)) = (get_rec A p m2).
+Proof.
+  intros.
+  unfold get_rec, get_rec'; fold get_rec'.
+  set (f1 := fun m0 : tree A => match m0 with
+                        | Leaf => Leaf
+                        | Node _ _ r => r
+                        end).
+  set (f2 := fun m0 : tree A => m0).
+  assert (forall m1 o m2, f1 (Node m1 o m2) = f2 m2).
+  subst; auto.
+  clearbody f1 f2.
+  revert f1 f2 H.
+  induction p; intros.
+  - unfold get_rec'; fold get_rec'.
+    apply IHp.
+    intros; rewrite H. auto.
+  -unfold get_rec'; fold get_rec'.
+    apply IHp.
+    intros; rewrite H. auto.
+  - compute. rewrite H. auto.
+Qed.
+
+  Lemma set_node0 : forall A p x m1 o m2,
+  (set_rec A p~0 x (Node m1 o m2)) = Node (set_rec A p x m1) o m2.
+Proof.
+  intros.
+  unfold set_rec, set_rec'; fold set_rec'.
+  set (f1 := fun hole : tree A => Node hole o m2) at 1.
+  set (f2 := fun hole : tree A => hole).
+  assert (forall t, f1 t = Node (f2 t) o m2).
+  subst; auto.
+  clearbody f1 f2.
+  revert m1 f1 f2 H.
+  induction p; intros.
+  - unfold set_rec'; fold set_rec'.
+    destruct m1.
+    + apply IHp. intros; apply H.
+    + apply IHp. intros; apply H.
+  - unfold set_rec'; fold set_rec'.
+    destruct m1.
+    + apply IHp. intros; apply H.
+    + apply IHp. intros; apply H.
+  - unfold set_rec'; fold set_rec'.
+    destruct m1.
+    + apply H.
+    + apply H.
+Qed.
+
+  Lemma set_node1 : forall A p x m1 o m2,
+  (set_rec A p~1 x (Node m1 o m2)) = Node m1 o (set_rec A p x m2).
+Proof.
+  intros.
+  unfold set_rec, set_rec'; fold set_rec'.
+  set (f1 := fun hole : tree A => Node m1 o hole) at 1.
+  set (f2 := fun hole : tree A => hole).
+  assert (forall t, f1 t = Node m1 o (f2 t)).
+  subst; auto.
+  clearbody f1 f2.
+  revert m2 f1 f2 H.
+  induction p; intros.
+  - unfold set_rec'; fold set_rec'.
+    destruct m2.
+    + apply IHp. intros; apply H.
+    + apply IHp. intros; apply H.
+  - unfold set_rec'; fold set_rec'.
+    destruct m2.
+    + apply IHp. intros; apply H.
+    + apply IHp. intros; apply H.
+  - unfold set_rec'; fold set_rec'.
+    destruct m2.
+    + apply H.
+    + apply H.
+Qed.
+
+  Lemma xelements_append : forall A m l1 l2,
+    xelements A m (l1 ++ l2) = xelements A m l1 ++ l2.
+Proof.
+    induction m; intros; simpl.
+  - auto.
+  - destruct o; rewrite IHm2; rewrite <- IHm1; auto.
+  Qed.
+
+  Lemma elements_set_none : forall A p x m,
   get_rec A p m = None ->
-  elements A m = l1 ++ l2 /\
+  exists l1 l2, elements A m = l1 ++ l2 /\
   elements A (set_rec A p x m) = l1 ++ (x :: l2).
 Proof.
-Admitted.
-(*
-  Lemma elements_set_some : forall A p x x' m l1 l2,
+  intros.
+  revert p H.
+  induction m.
+  - exists nil, nil.
+    split; simpl.
+    + auto.
+    + apply elements_set_empty.
+  - destruct p.
+    + rewrite get_node1; intros.
+      apply IHm2 in H; unfold elements in H.
+      destruct H as [s1]; destruct H as [s2]; destruct H as [H1 H2].
+      rewrite set_node1.
+      destruct o; unfold elements; unfold xelements; fold xelements.
+      * rewrite H1, H2.
+        exists (xelements A m1 (a :: s1)), s2.
+        split. apply (xelements_append A m1 (a :: s1) s2).
+        apply (xelements_append A m1 (a :: s1) (x :: s2)).
+      * rewrite H1, H2.
+        exists (xelements A m1 s1), s2.
+        split. apply (xelements_append A m1 s1 s2).
+        apply (xelements_append A m1 s1 (x :: s2)).
+    + rewrite get_node0; intros.
+      apply IHm1 in H; unfold elements in H.
+      destruct H as [s1]; destruct H as [s2]; destruct H as [H1 H2].
+      rewrite set_node0.
+      destruct o; unfold elements; unfold xelements; fold xelements.
+      * exists s1, (s2 ++ a :: xelements A m2 nil). 
+        rewrite app_comm_cons. repeat rewrite app_assoc. rewrite <- H1, <- H2.
+        split.
+        apply (xelements_append A m1 nil (a :: xelements A m2 nil)).
+        apply (xelements_append A (set_rec A p x m1) nil (a :: xelements A m2 nil)).
+      * exists s1, (s2 ++ xelements A m2 nil).
+        rewrite app_comm_cons. repeat rewrite app_assoc. rewrite <- H1, <- H2.
+        split.
+        apply (xelements_append A m1 nil (xelements A m2 nil)).
+        apply (xelements_append A (set_rec A p x m1) nil (xelements A m2 nil)).
+    + unfold get_rec, get_rec', set_rec, set_rec'.
+      intros. rewrite H.
+      unfold elements; unfold xelements; fold xelements.
+      exists (xelements A m1 nil), (xelements A m2 nil).
+      split.
+      apply (xelements_append A m1 nil (xelements A m2 nil)).
+      apply (xelements_append A m1 nil (x :: xelements A m2 nil)).
+Qed.
+
+  Lemma elements_set_some : forall A p x x' m,
   get_rec A p m = Some x ->
-  elements A m = l1 ++ (x :: l2) ->
+  exists l1 l2, elements A m = l1 ++ (x :: l2) /\
   elements A (set_rec A p x' m) = l1 ++ (x' :: l2).
 Proof.
-Admitted.
-*)
+  intros.
+  revert p H.
+  induction m.
+  - intros.
+    pose proof get_empty A p; unfold empty in H0.
+    congruence.
+  - destruct p.
+    + rewrite get_node1; intros.
+      apply IHm2 in H; unfold elements in H.
+      destruct H as [s1]; destruct H as [s2]; destruct H as [H1 H2].
+      rewrite set_node1.
+      destruct o; unfold elements; unfold xelements; fold xelements.
+      * rewrite H1, H2.
+        exists (xelements A m1 (a :: s1)), s2.
+        split. apply (xelements_append A m1 (a :: s1) (x :: s2)).
+        apply (xelements_append A m1 (a :: s1) (x' :: s2)).
+      * rewrite H1, H2.
+        exists (xelements A m1 s1), s2.
+        split. apply (xelements_append A m1 s1 (x :: s2)).
+        apply (xelements_append A m1 s1 (x' :: s2)).
+    + rewrite get_node0; intros.
+      apply IHm1 in H; unfold elements in H.
+      destruct H as [s1]; destruct H as [s2]; destruct H as [H1 H2].
+      rewrite set_node0.
+      destruct o; unfold elements; unfold xelements; fold xelements.
+      * exists s1, (s2 ++ a :: xelements A m2 nil). 
+        repeat rewrite app_comm_cons. repeat rewrite app_assoc.
+        rewrite <- H1, <- H2.
+        split.
+        apply (xelements_append A m1 nil (a :: xelements A m2 nil)).
+        apply (xelements_append A (set_rec A p x' m1) nil (a :: xelements A m2 nil)).
+      * exists s1, (s2 ++ xelements A m2 nil).
+        repeat rewrite app_comm_cons. repeat rewrite app_assoc.
+        rewrite <- H1, <- H2.
+        split.
+        apply (xelements_append A m1 nil (xelements A m2 nil)).
+        apply (xelements_append A (set_rec A p x' m1) nil (xelements A m2 nil)).
+    + unfold get_rec, get_rec', set_rec, set_rec'.
+      intros. rewrite H.
+      unfold elements; unfold xelements; fold xelements.
+      exists (xelements A m1 nil), (xelements A m2 nil).
+      split.
+      apply (xelements_append A m1 nil (x :: xelements A m2 nil)).
+      apply (xelements_append A m1 nil (x' :: xelements A m2 nil)).
+Qed.
+
 End PTree.
 
 Fixpoint mark_sort' tep m: PTree.tree expr :=
@@ -453,16 +564,31 @@ End AxiomClass2.
 
 Section Proof.
 
-Fixpoint build' l p: expr :=
-  match l with
-  | nil => p
-  | q :: l' => build' l' (p * q)
-  end.
 Fixpoint build l : expr :=
   match l with
   | nil => emp
-  | p :: l' => build' l' p
+  | p :: l' => p * build l'
   end.
+
+Lemma build_cons : forall l1 l2 e,
+  |-- build (l1 ++ e :: l2) --> build (l1 ++ l2) * e.
+Proof.
+  intros.
+  induction l1; simpl.
+  - apply sepcon_comm_impp.
+  - rewrite IHl1.
+    apply sepcon_assoc1.
+Qed.
+
+Lemma sepcon_build : forall l1 l2 e,
+  |-- build (l1 ++ l2) * e --> build (l1 ++ e :: l2).
+Proof.
+  intros.
+  induction l1; simpl.
+  - apply sepcon_comm_impp.
+  - rewrite <- IHl1.
+    apply sepcon_assoc2.
+Qed.
 
 Lemma add1 : forall e' p m,
   |-- build (PTree.elements expr (mark_sort' (var_pos e' (Some p)) m))
@@ -471,11 +597,21 @@ Proof.
   intros.
   unfold mark_sort'.
   destruct (PTree.get_rec expr p m) eqn:?H.
-  -(* pose proof (PTree.elements_set_some expr p e e' m).*)
-    
-    
-
-Admitted.
+  - pose proof (PTree.elements_set_some expr p e (e * e') m).
+    apply H0 in H; clear H0.
+    destruct H as [l1]; destruct H as [l2].
+    destruct H as [H1 H2].
+    rewrite H1, H2.
+    rewrite build_cons. rewrite <- sepcon_build.
+    apply sepcon_assoc1.
+  - pose proof (PTree.elements_set_none expr p e' m).
+    apply H0 in H; clear H0.
+    destruct H as [l1]; destruct H as [l2].
+    destruct H as [H1 H2].
+    rewrite H1, H2.
+    rewrite build_cons.
+    apply provable_impp_refl.
+Qed.
 
 Lemma add2 : forall e' p m,
   |-- build (PTree.elements expr m) * e'
@@ -483,7 +619,22 @@ Lemma add2 : forall e' p m,
 Proof.
   intros.
   unfold mark_sort'.
-Admitted.
+  destruct (PTree.get_rec expr p m) eqn:?H.
+  - pose proof (PTree.elements_set_some expr p e (e * e') m).
+    apply H0 in H; clear H0.
+    destruct H as [l1]; destruct H as [l2].
+    destruct H as [H1 H2].
+    rewrite H1, H2.
+    rewrite build_cons. rewrite <- sepcon_build.
+    apply sepcon_assoc2.
+  - pose proof (PTree.elements_set_none expr p e' m).
+    apply H0 in H; clear H0.
+    destruct H as [l1]; destruct H as [l2].
+    destruct H as [H1 H2].
+    rewrite H1, H2.
+    rewrite <- sepcon_build.
+    apply provable_impp_refl.
+Qed.
 
 Lemma L1_before1 : forall sp p,
   |-- sp --> build (PTree.elements expr (mark_sort (var_pos sp (Some p)))).
@@ -493,7 +644,7 @@ Proof.
   rewrite PTree.get_empty.
   rewrite PTree.elements_set_empty.
   compute.
-  apply provable_impp_refl.
+  apply sepcon_emp2.
 Qed.
 
 Lemma L1_before2 : forall p1 p2,
@@ -561,7 +712,7 @@ Proof.
   rewrite PTree.get_empty.
   rewrite PTree.elements_set_empty.
   compute.
-  apply provable_impp_refl.
+  apply sepcon_emp1.
 Qed.
 
 Lemma L2_before2 : forall q1 q2,
