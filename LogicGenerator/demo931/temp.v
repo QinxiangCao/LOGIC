d1476d57345c87447ea500b3d5ea99ee6d0f6863
TaoYC0904 <taoyc0904@sjtu.edu.cn>
2022-05-13 22:22:13 +0800

    tactics added in temp.v

--- LogicGenerator/demo931/temp.v
+++ LogicGenerator/demo931/temp.v
@@ -1,9 +1,9 @@
 Require Import Logic.LogicGenerator.demo931.impl1.
-(* Require Import Logic.LogicGenerator.demo931.Interface. *)
 Import T.
 
 (*
-1. coqprop P1 && coqprop P2 && ... coqprop Px && P |-- coqprop Q1 && coqprop Q2 && ... coqprop Qx && Q
+1. coqprop P1 && coqprop P2 && ... coqprop Px && P 
+  |-- coqprop Q1 && coqprop Q2 && ... coqprop Qx && Q
 2. coqprop P1 && coqprop P2 && ... coqprop Px && P |-- Q
 3. P |-- coqprop Q1 && coqprop Q2 && ... coqprop Qx && Q
 *)
@@ -33,6 +33,45 @@ Proof.
   intros. split; try tauto.
   apply H0; try tauto.
 Qed.
+
+Ltac pop_coq_prop :=
+  repeat match goal with
+  | |- derivable1 (andp (coq_prop ?P) _) _ => apply cp_lem1
+  | |- (_ -> (derivable1 _ (andp (coq_prop ?P') _))) => apply cp_lem2
+  | _ => idtac
+  end.
+
+Context {P : Prop} {P' : Prop} {Q : expr} {Q' : expr}.
+
+Goal derivable1 (andp (coq_prop P) Q) Q'.
+Proof. pop_coq_prop. Abort.
+
+Goal (P -> (derivable1 Q (andp (coq_prop P') Q'))).
+Proof. pop_coq_prop. Abort.
+
+Goal (derivable1 (andp (coq_prop P) Q) (andp (coq_prop P') Q')).
+Proof. pop_coq_prop. Abort.
+
+(* Ltac entailer :=
+  match goal with
+  | |- _ |-- _ => idtac
+  | _ => fail "The proof goal's conclusion is not an assertion derivation."
+  end;
+  assert_simpl;
+  apply FOL_complete;
+  let st := fresh "st" in
+  intros st;
+  cbv [Assertion_denote term_denote];
+  repeat
+    match goal with
+    | |- context [aexp_denote st (Concrete_Pretty_Printing.AId ?X)] =>
+           let X' := fresh X "'" in
+           set (X' := aexp_denote st (Concrete_Pretty_Printing.AId X));
+           clearbody X';
+           revert X'
+    end;
+  first [ clear st | fail 1 "This is not a concrete derivation." ]. *)
+
 
 
 
